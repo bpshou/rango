744ff1fd59dd6f15deb97f1f8a9612f907fb51ee
zhangzeli <zhangzeli@inmyshow.com>
2023-08-27 08:32:23 +0800

    跨域请求优化

--- middleware/CrossMiddleware.go
+++ middleware/CrossMiddleware.go
@@ -9,10 +9,13 @@ import (
 // 设置跨域
 func CrossSetHeader(c *gin.Context) {
 	method := c.Request.Method
-	c.Header("Access-Control-Allow-Origin", "*")
-	c.Header("Access-Control-Allow-Headers", "Content-Type, AccessToken, X-CSRF-Token, Authorization, Token")
-	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE") // 服务器支持的所有跨域请求的方式
-	c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+	c.Header("Access-Control-Allow-Origin", "http://127.0.0.1:90")
+	// 服务器支持跨域请求的方式
+	c.Header("Access-Control-Allow-Methods", "HEAD, POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+	// 客户端请求服务端所能携带header的名单
+	c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	// 客户端能拿到服务端响应header的名单
+	c.Header("Access-Control-Expose-Headers", "")
 	c.Header("Access-Control-Allow-Credentials", "true")
 	// 放行所有OPTIONS方法
 	if method == "OPTIONS" {
